8f4a624bdc9c309126f9365bebb3d9b7e9d1d4ef
Cobalt <97923189+Cobalt-II@users.noreply.github.com>
2023-01-23 21:07:56 -0500

    Update Minimap.js

--- Minimap.js
+++ Minimap.js
@@ -306,9 +306,11 @@ let leader = false;
 for (let count = 1; count <= 4; count++) {
     localStorage.setItem('array' + count, [])
 };
+
 for (let count = 1; count <= 4; count++) {
     localStorage.setItem('ids' + count, [])
 };
+
 let take = [];
 const table = ['#7adbbc', '#b9e87e', '#e7896d', '#fdf380', '#b58efd', '#ef99c3', '#e8ebf7', '#aa9f9e', '#ffffff', '#484848', '#3ca4cb', '#8abc3f', '#e03e41', '#efc74b', '#8d6adf', '#cc669c', '#a7a7af', '#726f6f', '#dbdbdb', '#000000'];
 let broad = new BroadcastParser();
@@ -318,11 +320,13 @@ let xs = 0;
 let ys = 0;
 let id = [];
 const world = new UpdateParser();
+
 document.addEventListener('keydown', function(event) {
     if (event.code === 'KeyX') {
         leader = !leader;
     };
 });
+
 Array.prototype.shift = new Proxy(Array.prototype.shift, {
     apply(shift, array, args) {
         if (array[0] === 'u') {
@@ -337,7 +341,9 @@ Array.prototype.shift = new Proxy(Array.prototype.shift, {
         if (array[0] === 'b') {
             broad.parse(array);
             if (!leader) {
+
                 let set = [];
+
                 for (let count in broad.teamMinimap) {
                     switch (broad.teamMinimap[count].color) {
                         case 10:
@@ -353,16 +359,18 @@ Array.prototype.shift = new Proxy(Array.prototype.shift, {
                             controller = 4;
                             break;
                     }
-                    
+
                     if (localStorage.getItem('ids' + controller).split(',').indexOf(`${broad.teamMinimap[count].id}`) !== -1) {
                         broad.teamMinimap[count].color = 3;
                     };
-                    
+
                     set.push(broad.teamMinimap[count].id, broad.teamMinimap[count].x, broad.teamMinimap[count].y, broad.teamMinimap[count].color);
                 };
                 set.splice(set.length - 4, set.length);
                 localStorage.setItem('array' + controller, set);
+
             } else {
+
                 localStorage.setItem('array' + controller, []);
                 take = [
                     [],
@@ -370,6 +378,7 @@ Array.prototype.shift = new Proxy(Array.prototype.shift, {
                     [],
                     []
                 ];
+
                 for (let count = 1; count <= 4; count++) {
                     let h = localStorage.getItem('array' + count).split(',');
                     for (let count = 0; count < h.length; count += 4) {
@@ -381,8 +390,10 @@ Array.prototype.shift = new Proxy(Array.prototype.shift, {
         return shift.apply(array, args);
     }
 });
+
 let colors = ["#3ca4cb", "#8abc3f", "#e03e41", "#cc669c", "#8d6adf", "#dbdbdb"];
 let dra = [];
+
 const rect = CanvasRenderingContext2D.prototype.fillRect
 CanvasRenderingContext2D.prototype.fillRect = function(...args) {
     for (let count in colors) {
@@ -392,8 +403,10 @@ CanvasRenderingContext2D.prototype.fillRect = function(...args) {
     }
     rect.call(this, ...args);
 };
+
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+
 requestAnimationFrame(function draw() {
     for (let co in take) {
         for (let count in take[co]) {
@@ -405,11 +418,13 @@ requestAnimationFrame(function draw() {
             ctx.fill();
         }
     };
+
     dra = dra.sort((a, b) => Math.hypot(a[0], a[1]) - Math.hypot(b[0], b[1]));
     xs = dra[0][0] / window.innerWidth;
     ys = dra[0][1] / window.innerHeight;
     miniWidth = dra[dra.length - 1][0] + dra[dra.length - 1][3] - dra[0][0];
     miniHeight = dra[dra.length - 1][1] + dra[dra.length - 1][3] - dra[0][1];
+
     dra.splice(0, dra.length);
     requestAnimationFrame(draw);
 });
