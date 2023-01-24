2509eefed053625e85d3f85c5c9d3793e16f1075
Cobalt <97923189+Cobalt-II@users.noreply.github.com>
2023-01-23 21:09:25 -0500

    Update FOV.js

--- FOV.js
+++ FOV.js
@@ -1,7 +1,7 @@
 Array.prototype.shift = new Proxy(Array.prototype.shift, {
     apply(shift, array, args) {
         if (array[0] === 'u') {
-            array[4] = array[4] + 500;
+            array[4] = array[4] * 1.5;
         };
         return shift.apply(array, args);
     }
